0a9477a149ab885589d57584319244ff1d71ab75
kzk-maeda <kzk.maeda0711@gmail.com>
2022-07-27 16:55:48 +0900

    search, filter

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -12,8 +12,28 @@ export default function Index({ data }) {
   );
 }
 
-export async function getServerSideProps() {
-  const res = await axios.get(`${process.env.API_URL}/api/jobs/`);
+export async function getServerSideProps({ query }) {
+  const keyword = query.keyword || "";
+  const location = query.location || "";
+  const jobType = query.jobType || "";
+  const education = query.education || "";
+  const experience = query.experience || "";
+
+  let min_salary = "";
+  let max_salary = "";
+
+  if (query.salary) {
+    const [min, max] = query.salary.split("-");
+    min_salary = min;
+    max_salary = max;
+  }
+
+  const page = query.page || 1;
+
+  const queryString = `keyword=${keyword}&location=${location}&page=${page}&jobType=${jobType}&education=${education}&experience=${experience}&min_salary=${min_salary}&max_salary=${max_salary}`;
+  // console.log(query);
+
+  const res = await axios.get(`${process.env.API_URL}/api/jobs?${queryString}`);
   const data = res.data;
 
   return {
